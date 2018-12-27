a4e7cbc54dc6108476c7ac4ce47364f0442dfbf5
Dharmvijay Patel <dharmvijay.patel@multidots.com>
2018-12-27 17:36:26 +0530

    try catch in register and logggers added in logout tests

--- tests/Browser/LogoutTest.php
+++ tests/Browser/LogoutTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Browser;
 
+use Illuminate\Support\Facades\Log;
+use PHPUnit\Framework\ExpectationFailedException;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Tests\Browser\Pages\LoginPage;
@@ -10,23 +12,48 @@ use Tests\Browser\Pages\LoginPage;
 class LogoutTest extends DuskTestCase
 {
     /**
-     * test login
+     * test logout
      * @throws \Throwable
      */
     public function testLogout()
     {
         $this->browse(function (Browser $browser) {
-            $browser
-                ->visit(LoginPage::LOGIN_URL)
-                ->type('email', LoginPage::USER_EMAIL)
-                ->type('password', LoginPage::USER_PASS)
-                ->press('Login')
-                ->waitForLocation(LoginPage::HOME_URL)
-                ->assertUrlIs(LoginPage::homeUrl())
-                ->assertSee(LoginPage::HOME_ASSERT_STRING_1)
-                ->assertSee(LoginPage::HOME_ASSERT_STRING_2)
-                ->click('#navbarDropdown')
-                ->click('a.dropdown-item')->pause(200);
+            try{
+                $browser->visit(LoginPage::LOGIN_URL);
+                Log::info('[testLogout]: Visited login page.');
+
+                $browser->type('email', LoginPage::USER_EMAIL);
+                Log::info('[testLogout]: Entered email address.');
+
+                $browser->type('password', LoginPage::USER_PASS);
+                Log::info('[testLogout]: Entered password.');
+
+                $browser->press('Login');
+                Log::info('[testLogout]: Hit login button to authenticate.');
+
+                $browser->waitForLocation(LoginPage::HOME_URL);
+                Log::info('[testLogout]: Wait until it authenticate and redirect to home page.');
+
+                $browser->assertUrlIs(LoginPage::homeUrl());
+                $browser->assertSee(LoginPage::HOME_ASSERT_STRING_1);
+                $browser->assertSee(LoginPage::HOME_ASSERT_STRING_2);
+                Log::info('[testLogout]: Assert current url is home page url and other content.');
+
+                $browser->click('#navbarDropdown');
+                $browser->click('a.dropdown-item')->pause(200);
+                Log::info('[testLogout]: Hit logout link.');
+
+                $browser->assertSee('Laravel');
+                Log::info('[testLogout]: Assert to see home content of "laravel".');
+
+            } catch (ExpectationFailedException $ex){
+                $this->exceptionLogging($ex);
+                throw $ex;
+            } catch (\Exception $ex) {
+                $this->exceptionLogging($ex);
+                throw $ex;
+            }
+
         });
     }
 }
--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -7,94 +7,126 @@ use PHPUnit\Framework\ExpectationFailedException;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Tests\Browser\Pages\RegisterPage;
+use Exception;
 
 
 class RegisterTest extends DuskTestCase
 {
+
     /**
      * test login validations
      * @throws \Throwable
      */
-
-    public function testRegisterUserValidation()
+    public function testRegisterUserFormValidation()
     {
         $this->browse(function (Browser $browser) {
-            $browser
-                ->visit(RegisterPage::REGISTER_URL)
-                ->press('Register')
-                ->pause(200)
-                ->assertSee('The name field is required.')
-                ->assertSee('The email field is required.')
-                ->assertSee('The password field is required.')->pause(200);
+            try{
+                $browser->visit(RegisterPage::REGISTER_URL);
+                $browser->press('Register');
+                $browser->pause(200);
+                $browser->assertSee('The name field is required.');
+                $browser->assertSee('The email field is required.');
+                $browser->assertSee('The password field is required.')->pause(200);
+            } catch (ExpectationFailedException $ex){
+                $this->exceptionLogging($ex);
+                throw $ex;
+            } catch (Exception $ex) {
+                $this->exceptionLogging($ex);
+                throw $ex;
+            }
         });
 
         $this->browse(function (Browser $browser) {
-            $browser
-                ->visit(RegisterPage::REGISTER_URL)
-                ->type('email', RegisterPage::USER_EMAIL)
-                ->press('Register')
-                ->pause(200)
-                ->assertSee('The password field is required.')->pause(200);
+            try{
+                $browser->visit(RegisterPage::REGISTER_URL);
+                $browser->type('email', RegisterPage::USER_EMAIL);
+                $browser->press('Register');
+                $browser->pause(200);
+                $browser->assertSee('The password field is required.')->pause(200);
+            } catch (ExpectationFailedException $ex){
+                $this->exceptionLogging($ex);
+                throw $ex;
+            } catch (Exception $ex) {
+                $this->exceptionLogging($ex);
+                throw $ex;
+            }
         });
 
         $this->browse(function (Browser $browser) {
-            $browser
-                ->visit(RegisterPage::REGISTER_URL)
-                ->type('email', RegisterPage::USER_EMAIL)
-                ->press('Register')
-                ->pause(200)
-                ->assertSee('The password field is required.')->pause(200);
+            try{
+                $browser->visit(RegisterPage::REGISTER_URL);
+                $browser->type('email', RegisterPage::USER_EMAIL);
+                $browser->press('Register');
+                $browser->pause(200);
+                $browser->assertSee('The password field is required.')->pause(200);
+            } catch (ExpectationFailedException $ex){
+                $this->exceptionLogging($ex);
+                throw $ex;
+            } catch (Exception $ex) {
+                $this->exceptionLogging($ex);
+                throw $ex;
+            }
         });
 
         $this->browse(function (Browser $browser) {
-            $browser
-                ->visit(RegisterPage::REGISTER_URL)
-                ->type('password', RegisterPage::USER_PASS)
-                ->press('Register')
-                ->pause(200)
-                ->assertSee('The email field is required.')->pause(200);
+            try{
+                $browser->visit(RegisterPage::REGISTER_URL);
+                $browser->type('password', RegisterPage::USER_PASS);
+                $browser->press('Register');
+                $browser->pause(200);
+                $browser->assertSee('The email field is required.')->pause(200);
+            } catch (ExpectationFailedException $ex){
+                $this->exceptionLogging($ex);
+                throw $ex;
+            } catch (Exception $ex) {
+                $this->exceptionLogging($ex);
+                throw $ex;
+            }
         });
 
         $this->browse(function (Browser $browser) {
-            $browser
-                ->visit(RegisterPage::REGISTER_URL)
-                ->type('email', RegisterPage::WRONG_USER_EMAIL)
-                ->type('password', RegisterPage::WRONG_USER_PASS)
-                ->press('Register')
-                ->pause(200)
-                ->assertSee('The name field is required.')->pause(200);
+            try{
+                $browser->visit(RegisterPage::REGISTER_URL);
+                $browser->type('email', RegisterPage::WRONG_USER_EMAIL);
+                $browser->type('password', RegisterPage::WRONG_USER_PASS);
+                $browser->press('Register');
+                $browser->pause(200);
+                $browser->assertSee('The name field is required.')->pause(200);
+            } catch (ExpectationFailedException $ex){
+                $this->exceptionLogging($ex);
+                throw $ex;
+            } catch (Exception $ex) {
+                $this->exceptionLogging($ex);
+                throw $ex;
+            }
         });
-
-
-
     }
 
     /**
-     * test login
+     * test registration
      * @throws \Throwable
      */
     public function testRegister()
     {
         try{
             $this->browse(function (Browser $browser) {
-                $browser
-                    ->visit(RegisterPage::REGISTER_URL)
-                    ->type('name', RegisterPage::USER_NAME)
-                    ->type('email', RegisterPage::USER_EMAIL)
-                    ->type('password', RegisterPage::USER_PASS)
-                    ->type('password_confirmation', RegisterPage::USER_PASS)
-                    ->press('Register')
-                    ->waitForLocation(RegisterPage::HOME_URL)
-                    ->assertUrlIs(RegisterPage::homeUrl())
-                    ->assertSee(RegisterPage::HOME_ASSERT_STRING_1)
-                    ->assertSee(RegisterPage::HOME_ASSERT_STRING_2)->pause(500);
+                $browser->visit(RegisterPage::REGISTER_URL);
+                $browser->type('name', RegisterPage::USER_NAME);
+                $browser->type('email', RegisterPage::USER_EMAIL);
+                $browser->type('password', RegisterPage::USER_PASS);
+                $browser->type('password_confirmation', RegisterPage::USER_PASS);
+                $browser->press('Register');
+                $browser->waitForLocation(RegisterPage::HOME_URL);
+                $browser->assertUrlIs(RegisterPage::homeUrl());
+                $browser->assertSee(RegisterPage::HOME_ASSERT_STRING_1);
+                $browser->assertSee(RegisterPage::HOME_ASSERT_STRING_2)->pause(500);
             });
         } catch (ExpectationFailedException $ex){
+            $this->exceptionLogging($ex);
             throw $ex;
-        } catch (\Exception $ex) {
+        } catch (Exception $ex) {
+            $this->exceptionLogging($ex);
             throw $ex;
         }
-
-
     }
 }
