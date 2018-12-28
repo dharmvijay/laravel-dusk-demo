629ef085e74ac5dc6df18e3106a7b46e23a0b1a8
Dharmvijay Patel <dharmvijay.patel@multidots.com>
2018-12-28 12:55:23 +0530

    logs added in registration

--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -22,11 +22,19 @@ class RegisterTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             try{
                 $browser->visit(RegisterPage::REGISTER_URL);
+                Log::info('[testRegisterUserFormValidation]: Visited registration page.');
+
                 $browser->press('Register');
+                Log::info('[testRegisterUserFormValidation]: Hit register button to authenticate.');
+
                 $browser->pause(200);
                 $browser->assertSee('The name field is required.');
                 $browser->assertSee('The email field is required.');
                 $browser->assertSee('The password field is required.')->pause(200);
+                Log::info('[testRegisterUserFormValidation]: Validation message assertions seems fine.');
+                Log::info('[testRegisterUserFormValidation]: Field Validations executed successfully.');
+
+
             } catch (ExpectationFailedException $ex){
                 $this->exceptionLogging($ex);
                 throw $ex;
@@ -39,10 +47,16 @@ class RegisterTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             try{
                 $browser->visit(RegisterPage::REGISTER_URL);
+                Log::info('[testRegisterUserFormValidation]: Visited registration page.');
+
                 $browser->type('email', RegisterPage::USER_EMAIL);
                 $browser->press('Register');
+                Log::info('[testRegisterUserFormValidation]: Hit register button to authenticate.');
+
                 $browser->pause(200);
                 $browser->assertSee('The password field is required.')->pause(200);
+                Log::info('[testRegisterUserFormValidation]: Register form validation assertions seems fine.');
+
             } catch (ExpectationFailedException $ex){
                 $this->exceptionLogging($ex);
                 throw $ex;
@@ -55,10 +69,16 @@ class RegisterTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             try{
                 $browser->visit(RegisterPage::REGISTER_URL);
+                Log::info('[testRegisterUserFormValidation]: Visited registration page.');
+
                 $browser->type('email', RegisterPage::USER_EMAIL);
                 $browser->press('Register');
+                Log::info('[testRegisterUserFormValidation]: Hit register button to authenticate.');
+
                 $browser->pause(200);
                 $browser->assertSee('The password field is required.')->pause(200);
+                Log::info('[testRegisterUserFormValidation]: Register form validation assertions seems fine.');
+
             } catch (ExpectationFailedException $ex){
                 $this->exceptionLogging($ex);
                 throw $ex;
@@ -71,10 +91,16 @@ class RegisterTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             try{
                 $browser->visit(RegisterPage::REGISTER_URL);
+                Log::info('[testRegisterUserFormValidation]: Visited registration page.');
+
                 $browser->type('password', RegisterPage::USER_PASS);
                 $browser->press('Register');
+                Log::info('[testRegisterUserFormValidation]: Hit register button to authenticate.');
+
                 $browser->pause(200);
                 $browser->assertSee('The email field is required.')->pause(200);
+                Log::info('[testRegisterUserFormValidation]: Register form validation assertions seems fine.');
+
             } catch (ExpectationFailedException $ex){
                 $this->exceptionLogging($ex);
                 throw $ex;
@@ -87,11 +113,17 @@ class RegisterTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             try{
                 $browser->visit(RegisterPage::REGISTER_URL);
+                Log::info('[testRegisterUserFormValidation]: Visited registration page.');
+
                 $browser->type('email', RegisterPage::WRONG_USER_EMAIL);
                 $browser->type('password', RegisterPage::WRONG_USER_PASS);
                 $browser->press('Register');
+                Log::info('[testRegisterUserFormValidation]: Hit register button to authenticate.');
+
                 $browser->pause(200);
                 $browser->assertSee('The name field is required.')->pause(200);
+                Log::info('[testRegisterUserFormValidation]: Register form validation assertions seems fine.');
+
             } catch (ExpectationFailedException $ex){
                 $this->exceptionLogging($ex);
                 throw $ex;
@@ -100,6 +132,8 @@ class RegisterTest extends DuskTestCase
                 throw $ex;
             }
         });
+        Log::info('[testRegister]: Validate Registration test cases executed successfully.');
+
     }
 
     /**
@@ -111,15 +145,26 @@ class RegisterTest extends DuskTestCase
         try{
             $this->browse(function (Browser $browser) {
                 $browser->visit(RegisterPage::REGISTER_URL);
+                Log::info('[testRegister]: Visited registration page.');
+
                 $browser->type('name', RegisterPage::USER_NAME);
                 $browser->type('email', RegisterPage::USER_EMAIL);
                 $browser->type('password', RegisterPage::USER_PASS);
                 $browser->type('password_confirmation', RegisterPage::USER_PASS);
+                Log::info('[testRegister]: Entered all valid details as exacting it register successfully.');
+
                 $browser->press('Register');
+                Log::info('[testRegister]: Hit register button to authenticate.');
+
                 $browser->waitForLocation(RegisterPage::HOME_URL);
+                Log::info('[testRegister]: Wait until registration done and redirect to home page.');
+
                 $browser->assertUrlIs(RegisterPage::homeUrl());
                 $browser->assertSee(RegisterPage::HOME_ASSERT_STRING_1);
                 $browser->assertSee(RegisterPage::HOME_ASSERT_STRING_2)->pause(500);
+                Log::info('[testRegister]: Registration assertions seems fine.');
+                Log::info('[testRegister]: Registration test cases executed successfully.');
+
             });
         } catch (ExpectationFailedException $ex){
             $this->exceptionLogging($ex);
